2c1bb9b662d962eb9a800b765706986b97758bf8
tarotez <tez@sings.jp>
2016-12-21 17:37:49 +0900

    added sample3.cpp

---
+++ sample3.cpp
@@ -0,0 +1,51 @@
+/* sample 3 */
+#include <iostream>
+using namespace std;
+
+class person{
+public:
+  string address;
+  string name; 
+};
+
+class addressbook{
+  person friends[100];
+  int numberOfFriends;
+public:
+  addressbook();
+  void listFriends();
+  void addFriend(string name);
+};
+
+// constructor
+addressbook::addressbook(){
+  numberOfFriends = 0;  
+}
+
+void addressbook::addFriend(string name){
+  friends[numberOfFriends].name = name;
+  numberOfFriends++;
+}
+
+void addressbook::listFriends(){
+  for(int i = 0; i < numberOfFriends; i++){
+    cout << friends[i].name << "\n";
+  }      
+}
+
+int main()
+{
+  addressbook abook;
+  string name;
+
+  while(1){
+    cout << "住所録に登録する名前を入力してください（終了するにはquitと入力してください）: ";
+    cin >> name;
+    //   cout << "name.length() = " << name.length() << "\n";
+    if(name == "quit"){break;}
+    abook.addFriend(name);
+  }
+
+  cout << "\n名前リスト:\n";
+  abook.listFriends();  
+}
